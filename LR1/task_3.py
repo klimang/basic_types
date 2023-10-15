5cd0c0d0f838b7bcb9cf67fbf07fc93c00f76e77
klimang <79804329+klimang@users.noreply.github.com>
2023-10-15 20:15:48 +0300

    task_3.py

--- LR1/task_3.py
+++ LR1/task_3.py
@@ -1,6 +1,6 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
-half_of_players = len(list_players) // 2
+half_of_players = len(list_players)
 
 first_team = list_players[:half_of_players]
 second_team = list_players[half_of_players:]
