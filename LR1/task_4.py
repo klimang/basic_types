84e0ee51a57ae41d60491af8f2e9e6b96fa39331
klimang <79804329+klimang@users.noreply.github.com>
2023-10-15 20:13:02 +0300

    task_4.py

--- LR1/task_4.py
+++ LR1/task_4.py
@@ -1,5 +1,9 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
-
-stat = {"Общее количество": len(users), "Уникальные посещения": len(set(users))}
+stat = {
+    "Общее количество": 0,
+    "Уникальные посещения": 0
+}
+stat["Общее количество"] = len(users)
+stat["Уникальные посещения"] = len(set(users))
 
 print(stat)
